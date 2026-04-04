145261d1925f10ef2f87bd57807bd9a7b76b6c20
BoberUnity2023 <boberberezin@gmail.com>
2026-04-04 16:29:56 +0300

    Индикатор ракет: визуализация ракет

--- Assets/Scripts/UI/Race/IndicatorRocket.cs
+++ Assets/Scripts/UI/Race/IndicatorRocket.cs
@@ -4,11 +4,17 @@ using UnityEngine.UI;
 public class IndicatorRocket : MonoBehaviour
 {
     [SerializeField] private Hub _hub;
-    [SerializeField] private Text _indicator;
+    //[SerializeField] private Text _indicator;
+    [SerializeField] private GameObject[] _rockets;
 
     private void Update()
     {
         int count = _hub.Level.Race.Car.RocketGun.Armo;
-        _indicator.text = "Rocket: " + count.ToString() + "/4";
+        //_indicator.text = "Rocket: " + count.ToString() + "/4";
+
+        for (int i = 0; i < _rockets.Length; i++)
+        {
+            _rockets[i].SetActive(count > i);
+        }
     }
 }
