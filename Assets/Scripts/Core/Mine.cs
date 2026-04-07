5b851e37a63a1bd6877504f7a613d885edce2fd4
BoberUnity2023 <boberberezin@gmail.com>
2026-04-07 10:07:03 +0300

    Добавлены мины

---
+++ Assets/Scripts/Core/Mine.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class Mine : MonoBehaviour
+{
+    [SerializeField] private GameObject _blastPrefab;
+    [SerializeField] private float _collisionForce;
+    [Range(0, 100)][SerializeField] private int _createChance;    
+
+    private void Awake()
+    {
+        SetActive();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Car")
+        {
+            Car car = other.GetComponentInParent<Car>();
+            Crash(car);
+        }
+    }
+
+    private void Crash(Car car)
+    {
+        if (!car.IsVisible)
+            return;
+        Instantiate(_blastPrefab, transform.position, Quaternion.identity);
+
+        Vector3 direction = (car.transform.position - transform.position).normalized;
+        car.Rigidbody.AddForce(direction * _collisionForce);
+        car.DamageCounter.DamageAdd(34);
+
+        gameObject.SetActive(false);
+    }
+
+    private void SetActive()
+    {
+        int rnd = Random.Range(0, 100);
+
+        if (rnd > _createChance)
+            gameObject.SetActive(false);
+    }
+}
