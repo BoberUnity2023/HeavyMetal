58a441ccf882c7e15c811bce2dc5812bfb0c2dc3
BoberUnity2023 <boberberezin@gmail.com>
2026-06-18 02:01:30 +0300

    Alien 6-7 уровень. Снег

---
+++ Assets/Scripts/Race/CopyPosition.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CopyPosition : MonoBehaviour
+{
+    [SerializeField] private Transform target = null;
+
+    private void FixedUpdate()
+    {
+        transform.position = target.position;
+        //transform.rotation = target.rotation;
+    }
+}
