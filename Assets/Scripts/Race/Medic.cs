a1ad4869f150b0f8a8f9748fa98148e0aa518319
BoberUnity2023 <boberberezin@gmail.com>
2026-08-31 16:36:42 +0300

    Здоровье. Подбирается игроком всегда.

--- Assets/Scripts/Race/Medic.cs
+++ Assets/Scripts/Race/Medic.cs
@@ -7,7 +7,7 @@ public class Medic : MonoBehaviour
         if (other.gameObject.tag == "Car")
         {
             Car car = other.GetComponentInParent<Car>();
-            if (car.DamageCounter.Damage > 0)
+            if (car.DamageCounter.Damage > 0 || !car.IsAI)
             {
                 car.DamageCounter.Restart();
                 gameObject.SetActive(false);
